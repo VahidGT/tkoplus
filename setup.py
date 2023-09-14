4dabec03f4e8d150cbe1a84eea8517db2f2f96fb
VahidGT <144277324+VahidGT@users.noreply.github.com>
2023-09-14 23:00:00 +0330

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="tkoplus",
+    version= "0.1",
+    description='tkinter option plus',
+    author="Vahid",
+    author_email="vahidpy3@gmail.com",
+    packages=['tkoplus']
+)
